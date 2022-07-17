cdc99aea35f6cc6fa499796a3a47f84cbef61398
asad siddiquee <asadsidn@gmail.com>
2022-07-17 21:31:26 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import GoogleAnalytics from "../components/";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      {process.env.NODE_ENV === "production" && <GoogleAnalytics />}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
