0d5e72d4702a79d0d9890e1ea7626fb81a0e72de
asad siddiquee <asadsidn@gmail.com>
2022-08-19 03:04:29 +0200

    ads

--- pages/_document.js
+++ pages/_document.js
@@ -33,6 +33,11 @@ export default function Document() {
             })
                 `}
         </Script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8098281641773267"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
