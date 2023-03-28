34477c7a0742aa6a026e50ed04163be1337d8bcc
asad <asadsidn@gmail.com>
2023-03-29 00:14:49 +0200

    scroll fixed

--- components/pagination/Pagination.jsx
+++ components/pagination/Pagination.jsx
@@ -1,4 +1,14 @@
+import React, { useEffect } from 'react';
+
 const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
+  useEffect(() => {
+    // 👇️ scroll to top on page load
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }, []);
+
+  function Scroll() {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
@@ -11,7 +21,10 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
       {pageNumbers.map((number) => (
         <div key={number} className="cursor-pointer">
           <div
-            onClick={() => paginate(number)}
+            onClick={() => {
+              paginate(number);
+              Scroll();
+            }}
             className={
               number == currentPage
                 ? `text-cyan-900 underline hover:text-cyan-900 text-xl sm:text-2xl`
